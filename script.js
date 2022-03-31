b4304ae1847862338f127d18350f692d520e3bcf
Mirjam Wolf <mwolf14@cnm.edu>
2022-03-30 22:47:53 -0600

    added more values for colors

--- script.js
+++ script.js
@@ -14,7 +14,14 @@ function initColorPicker() {
 
 function setColorPickerEventListeners(colorBox, rgb) {
     rgb.red.addEventListener('change', () => {
+        console.log("Red value: ", rgb.red.value);
     })
-    console.log("Red value: ", rgb.red.value);
-    };
+    rgb.green.addEventListener('change', () => {
+        console.log("Green value: ", rgb.green.value);
+    })
+    rgb.blue.addEventListener('change', () => {
+        console.log("Blue value: ", rgb.blue.value);
+    })
+
+}
 
